6ad4da8a98d9952d01ab1b3f0e76e25cd1254252
Risaf.R <risafr123@gmail.com>
2026-01-29 19:55:25 +0530

    Alarm clock using Python

--- alarm_clock.py
+++ alarm_clock.py
@@ -3,7 +3,7 @@ import time
 import datetime
 import pygame
 def set_alarm():
-    sound='ayooo-sayip-op.mp3'
+    sound='fire_alarm.mp3'
     is_True=True
     while is_True:
         crt_time=datetime.datetime.now().strftime('%H:%M:%S')
